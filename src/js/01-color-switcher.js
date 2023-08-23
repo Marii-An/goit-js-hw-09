f743f00403968214f835b6c2f93032c2a872f4d5
Marii-An <mariia.antonova@gmail.com>
2023-08-23 11:54:18 +0200

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const changeColor = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+stopBtn.setAttribute('disabled', '');
+
+startBtn.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+
+  intervalId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    changeColor.style.backgroundColor = randomColor;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
+
+  clearInterval(intervalId);
+});
